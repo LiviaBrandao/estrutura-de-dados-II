9fa6fc56cf9d658435c0cfcd898e07a7efca02e6
Livia Brandao <brandao.m.livia@gmail.com>
2021-11-14 19:50:54 -0300

    Final fibonacci version

--- src/program.java
+++ src/program.java
@@ -1,7 +1,17 @@
+import exercises.recursion.FibonacciSequence;
+
 public class program {
 
     public static void main (String[] args) {
 
+        try {
+
+            System.out.println("\n ------ Fibonacci -------- ");
+            System.out.println(FibonacciSequence.fibonacciFormated(8));
+
+        } catch (Exception err) {
+            System.err.println(err.getStackTrace());
+        }
 
     }
 }
